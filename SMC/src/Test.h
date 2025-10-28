e373e2c151d7abaea483793eb6c9c87ac6d0a29b
LAZARUS-inq <dev_3dlevel@outlook.com>
2025-10-29 01:48:23 +0800

    Get started (Setting up the project).

---
+++ SMC/src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace SMC {
+
+	__declspec(dllexport) void Print();
+}
---
+++ Sandbox/Application.cpp
@@ -0,0 +1,10 @@
+
+namespace SMC {
+
+	__declspec(dllimport) void Print();
+}
+
+void main()
+{
+	SMC::Print();
+}
